da966b35ffcf0ce0501d5a2999bbebf3c507b611
retrojack <jeffers.jake@gmail.com>
2025-07-14 19:10:28 -0400

    Moved external includes to new dir

--- include/OpenGL/utils.cpp
+++
@@ -1,8 +0,0 @@
-#include <utils.h>
-
-void checkGLError(const std::string& message) {
-	GLenum error = glGetError();
-	if (error != GL_NO_ERROR) {
-		std::cerr << "OpenGL Error: " << message << ". Error code: " << error << std::endl;
-	}
-}
--- include/OpenGL/utils.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include<glad/glad.h>
-
-void checkGLError(const std::string& message);
